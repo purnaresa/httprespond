bbf4ddaa2235f9915564594db5f1e58766d5c866
purnaresa <purnaresa@gmail.com>
2017-01-20 16:29:32 +0700

    initial

---
+++ httprespond.go
@@ -0,0 +1,42 @@
+package httprespond
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Response struct {
+	Data     interface{}   `json:"data,omitempty"`
+	Errors   []interface{} `json:"errors,omitempty"`
+	Meta     interface{}   `json:"meta,omitempty"`
+	Links    Links         `json:"links,omitempty"`
+	Included interface{}   `json:"included,omitempty"`
+}
+
+type Links struct {
+	Self       string      `json:"self,omitempty"`
+	Related    interface{} `json:"related,omitempty"`
+	Pagination Pagination  `json:"pagination,omitempty"`
+}
+
+type Pagination struct {
+	First string `json:"first,omitempty"`
+	Last  string `json:"last,omitempty"`
+	Prev  string `json:"prev,omitempty"`
+	Next  string `json:"next,omitempty"`
+}
+
+func Success(data interface{}, meta interface{}, included interface{}, pagination Pagination, w http.ResponseWriter, r *http.Request) {
+	response := Response{
+		Data:     data,
+		Meta:     meta,
+		Included: included,
+		Links: Links{
+			Self:       r.URL.Path,
+			Pagination: pagination,
+		},
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+}
